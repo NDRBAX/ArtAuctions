6b65e71534b5e62207c503534577fb2df9cbe012
NDRBAX <andre.bacaliu@icloud.com>
2022-09-15 20:28:19 +0200

    Models minor fixes done

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from multiselectfield import MultiSelectField
 
 
 class User(AbstractUser):
@@ -13,7 +14,7 @@ class Listing(models.Model):
     year = models.IntegerField(default=0)
     size = models.CharField(max_length=64, default="Unknown")
     avatar = models.URLField()
-    category = models.CharField(max_length=250, blank=True)         
+    category = models.CharField(max_length=250, blank=True)             
     price = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
     open_price = models.DecimalField(max_digits=10, decimal_places=0, blank=True)
     bid_count = models.IntegerField(default=0)
@@ -21,10 +22,10 @@ class Listing(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
     buyer = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-  
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.title} by {self.artist}"
+
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_bids")
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
